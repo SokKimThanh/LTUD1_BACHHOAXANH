13056b93425cc19ecbd21a2362251d143d8b95fd
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-11-16 12:33:48 +0700

    Xoa using khong su dung

--- ScreenMenu/Nhap/KhachHang/FormKhachHang.cs
+++ ScreenMenu/Nhap/KhachHang/FormKhachHang.cs
@@ -1,15 +1,5 @@
-﻿using LTUD1_MF_BHX.Connection;
-using LTUD1_MF_BHX.controller;
+﻿using LTUD1_MF_BHX.controller;
 using LTUD1_MF_BHX.Model;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace LTUD1_MF_BHX.ScreenMenu.Nhap
 {
