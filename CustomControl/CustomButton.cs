4737032641da7b9747c185f2d21b0421f67788f5
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-11-16 12:33:12 +0700

    Add files Custom Control

---
+++ CustomControl/CustomButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTUD1_MF_BHX.CustomControl
+{
+    public class CustomButton : Button
+    {
+        public CustomButton(string text, EventHandler eventHandler)
+        {
+            Text = text;
+            Click += eventHandler;
+        }
+    }
+}
