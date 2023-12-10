54451113a9cb51876323a36feb3003f0dc33f12b
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-12-10 22:30:25 +0700

    Giảm số lượng id

--- Model/GenerateID.cs
+++ Model/GenerateID.cs
@@ -14,12 +14,12 @@ namespace LTUD1_MF_BHX.Model
         /// <param name="prefix">ví dụ: nv0312312</param>
         /// <param name="id_cuoi">ví dụ: 01232123</param>
         /// <returns></returns>
-        public static string generateID(string prefix, string id_cuoi)
+        public static string generateID(string prefix, string id_cuoi, int startinxdex = 2)
         {
             // idIncrease: bỏ 2 kí tự đầu và chuyển sang số, rồi cộng lên 1
-            int idIncrease = Convert.ToInt32(id_cuoi.Trim().Substring(2)) + 1;
+            int idIncrease = Convert.ToInt32(id_cuoi.Trim().Substring(startinxdex)) + 1;
             // newid: định dạng số kết quả thành 9 số dạng chuỗi
-            string newid = prefix + string.Format("{0:000000000}", idIncrease);
+            string newid = prefix + string.Format("{0:0000}", idIncrease);
             // newid: trả về kết quả
             return newid;//ví dụ nv0123456789
         }
