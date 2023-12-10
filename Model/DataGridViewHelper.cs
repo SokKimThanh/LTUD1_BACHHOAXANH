08ffb60b3c3f7f19f67440d65908bd45c3268b39
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-12-10 22:42:54 +0700

    add files datagridview xử lý vẽ lại datagridview chung

---
+++ Model/DataGridViewHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTUD1_MF_BHX.Model
+{
+    public static class DataGridViewHelper
+    {
+        public static void ConfigureDataGridView(DataGridView dgv)
+        {
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgv.ForeColor = Color.Black;
+            dgv.ReadOnly = true;
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv.MultiSelect = false;
+            dgv.Dock = DockStyle.Fill;
+        }
+    }
+
+}
